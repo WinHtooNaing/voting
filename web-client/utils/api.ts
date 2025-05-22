197694fa87ecb33778f7144a0b363acded6b3706
Win Htoo Naing <winhtoonaing2003@gmail.com>
2025-05-22 21:05:59 +0630

    api add

--- web-client/utils/api.ts
+++ web-client/utils/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  baseURL:
+    process.env.NEXT_PUBLIC_SERVER_URL || "https://voting-nogy.onrender.com",
 });
 
 export default api;
